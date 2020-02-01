b8fb485b883aba3db1f9dae683358213c9327d28
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-02-01 03:59:15 +0300

    Application Completed

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# calculator
+# Calculator | Vue JS
+
+> A simple calculator developed with vue js
+
+
+![screencapture-mustafadalga-github-io-Front-End-Developments-Calculator-2020-02-01-00_51_58](https://user-images.githubusercontent.com/25087769/73577370-b2db1080-448d-11ea-86b9-149dcb08fa27.png)
+
+
+
+## Live Project :[Calculator](https://mustafadalga.github.io/Front-End-Developments/Calculator/) 
+
 
 ## Project setup
 ```
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,134 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="calculator">
+    <div class="display">{{ current || "0" }}</div>
+    <div class="btn" @click="clear">C</div>
+    <div class="btn" @click="sign">+/-</div>
+    <div class="btn" @click="percent">%</div>
+    <div class="btn operator" @click="divide">÷</div>
+    <div class="btn" @click="append('7')">7</div>
+    <div class="btn" @click="append('8')">8</div>
+    <div class="btn" @click="append('9')">9</div>
+    <div class="btn operator" @click="times">x</div>
+    <div class="btn" @click="append('4')">4</div>
+    <div class="btn" @click="append('5')">5</div>
+    <div class="btn" @click="append('6')">6</div>
+    <div class="btn operator" @click="subtract">-</div>
+    <div class="btn" @click="append('1')">1</div>
+    <div class="btn" @click="append('2')">2</div>
+    <div class="btn" @click="append('3')">3</div>
+    <div class="btn operator" @click="add">+</div>
+    <div class="btn zero" @click="zero">0</div>
+    <div class="btn" @click="dot">.</div>
+    <div class="btn operator" @click="equal">=</div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      previous: null,
+      current: "",
+      operator: null,
+      operatorClicked: false
+    };
+  },
+  methods: {
+    clear() {
+      this.current = "";
+    },
+    sign() {
+      this.current =
+        this.current.charAt(0) === "-"
+          ? this.current.slice(1)
+          : `-${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) {
+      if (this.operatorClicked) {
+        this.current = "";
+        this.operatorClicked = false;
+      }
+      this.current = `${this.current}${number}`;
+    },
+    dot() {
+      if (this.current.indexOf(".") === -1) {
+        this.append(".");
+      }
+    },
+    zero() {
+      if (!(this.current.length === 1 && this.current.indexOf(0) === 0)) {
+        this.append("0");
+      }
+    },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.operator = (number1, number2) => number1 / number2;
+      this.setPrevious();
+    },
+    times() {
+      this.operator = (number1, number2) => number1 * number2;
+      this.setPrevious();
+    },
+    subtract() {
+      this.operator = (number1, number2) => number1 - number2;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (number1, number2) => number1 + number2;
+      this.setPrevious();
+    },
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.previous),
+        parseFloat(this.current)
+      )}`;
+      this.previous = null;
+    }
   }
 };
 </script>
+
+<style>
+body {
+  background: #40c4ff;
+}
+.calculator {
+  margin: 0 auto;
+  width: 400px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+  font-size: 40px;
+  text-align: center;
+}
+.display {
+  grid-column: 1/5;
+  background-color: #333;
+  color: #fff;
+  padding: 1vmin;
+}
+.zero {
+  grid-column: 1/3;
+}
+.btn {
+  background-color: #f2f2f2;
+  border: 1px solid #999;
+  cursor: pointer;
+  -webkit-transition: all 0.3s;
+  -moz-transition: all 0.3s;
+  transition: all 0.3s;
+}
+.btn:active {
+  filter: brightness(0.75);
+}
+.operator {
+  background-color: #33691e;
+  color: #fff;
+}
+</style>
